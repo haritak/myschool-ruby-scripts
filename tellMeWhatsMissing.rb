f367920b79d2cb9e77eed33d015fcb5a591e5af3
Charitakis Yannis <github@ych.gr>
2016-05-18 12:56:46 +0300

    Moved to sinatra, as the main frontent

--- tellMeWhatsMissing.rb
+++ tellMeWhatsMissing.rb
@@ -1,131 +1,143 @@
 require 'spreadsheet'
+require 'sinatra'
+require 'haml'
 
-require 'pry'
+get '/upload' do
+  haml :upload
+end
 
-Spreadsheet.client_encoding = 'UTF-8'
+post '/upload' do
+  File.open('uploads/' + params['mine'][:filename], "w") do |f|
+    f.write(params['mine'][:tempfile].read)
+  end
 
-book = Spreadsheet.open './seleniumDownloads/rptList.xls'
+  Spreadsheet.client_encoding = 'UTF-8'
 
-sheet1 = book.worksheet 0
+  book = Spreadsheet.open "uploads/#{params['mine'][:filename]}"
 
-@STATE = 0
+  sheet1 = book.worksheet 0
 
-@debug = false
+  @STATE = 0
 
-@ignoreMissingTeacher = true
+  @debug = false
 
-@checkA = false
-@checkB = true
-@checkC = false
+  @ignoreMissingTeacher = true
 
-missingStudents= {}
-missingLessons = {}
-missingTmimata = {}
-missingLessons = {}
-sheet1.each_with_index do |row,r|
-  puts r if @debug
-  column = 0
-  row.each_with_index do |cell,i|
-    cell = cell.strip unless (not cell) or (not cell.respond_to?(:strip))
-    if @STATE == 0
-      if cell =~ /Τμήμα/
-        print "Βρήκα τμήμα " if @debug
-        @tmima=row[8]
-        puts @tmima if @debug
-      elsif cell =~ /Μάθημα/
-        print "Βρήκα μάθημα " if @debug
-        @mathima=row[5]
-        puts @mathima if @debug
-      elsif cell =~ /Διδ.* Εκπ.*/
-        print "Βρήκα και τους καθηγητές" if @debug
-        @teacher=row[5]
-        puts @teacher if @debug
-      elsif cell =~ /Α\/Α/
-        puts "Εδω ξεκινάνε οι μαθητές" if @debug
-        raise "error1" unless @tmima
-        raise "error2" unless @mathima
-        if not @ignoreMissingTeacher 
-          raise "error3" unless @teacher
+  p params
+  sem = params['semester']
+  @checkA = (sem=="1")
+  @checkB = (sem=="2")
+  @checkC = (sem=="3")
+
+  @missingStudents= {}
+  @missingLessons = {}
+  @missingTmimata = {}
+  sheet1.each_with_index do |row,r|
+    puts r if @debug
+    column = 0
+    row.each_with_index do |cell,i|
+      cell = cell.strip unless (not cell) or (not cell.respond_to?(:strip))
+      if @STATE == 0
+        if cell =~ /Τμήμα/
+          print "Βρήκα τμήμα " if @debug
+          @tmima=row[8]
+          puts @tmima if @debug
+        elsif cell =~ /Μάθημα/
+          print "Βρήκα μάθημα " if @debug
+          @mathima=row[5]
+          puts @mathima if @debug
+        elsif cell =~ /Διδ.* Εκπ.*/
+          print "Βρήκα και τους καθηγητές" if @debug
+          @teacher=row[5]
+          puts @teacher if @debug
+        elsif cell =~ /Α\/Α/
+          puts "Εδω ξεκινάνε οι μαθητές" if @debug
+          raise "error1" unless @tmima
+          raise "error2" unless @mathima
+          if not @ignoreMissingTeacher 
+            raise "error3" unless @teacher
+          end
+          raise "error4" unless @aTet
+          raise "error4" unless @bTet
+          raise "error4" unless @grapta
+
+          @withDegree = 0
+          @STATE=1
+        elsif cell =~ /Α Τετ.*/
+          @aTet = i
+          puts "A tetramino at #{@aTet}" if @debug
+        elsif cell =~ /Β Τετ.*/
+          @bTet = i
+          puts "B tetramino at #{@bTet}" if @debug
+        elsif cell =~ /Γραπ.*/
+          @grapta = i
+          puts "grapta at #{@grapta}" if @debug
         end
-        raise "error4" unless @aTet
-        raise "error4" unless @bTet
-        raise "error4" unless @grapta
+      elsif @STATE==1
+        puts "Δεν βρήκα το Α Τετράμηνο" unless @aTet
+        puts "Δεν βρήκα το Β Τετράμηνο" unless @bTet
+        puts "Δεν βρήκα τα γραπτα" unless @grapta
+        if i==0 && (cell =~ // or not cell)
+          @STATE=0
+          puts "Εδώ τελειώνουν οι μαθητές" if @debug
 
-        @missingCounter = 0
-        @STATE=1
-      elsif cell =~ /Α Τετ.*/
-        @aTet = i
-        puts "A tetramino at #{@aTet}" if @debug
-      elsif cell =~ /Β Τετ.*/
-        @bTet = i
-        puts "B tetramino at #{@bTet}" if @debug
-      elsif cell =~ /Γραπ.*/
-        @grapta = i
-        puts "grapta at #{@grapta}" if @debug
-      end
-    elsif @STATE==1
-      puts "Δεν βρήκα το Α Τετράμηνο" unless @aTet
-      puts "Δεν βρήκα το Β Τετράμηνο" unless @bTet
-      puts "Δεν βρήκα τα γραπτα" unless @grapta
-      if i==0 && (cell =~ // or not cell)
-        @STATE=0
-        puts "Εδώ τελειώνουν οι μαθητές" if @debug
-        @tmima = @mathima = @teacher = nil
-      elsif i>0
-        next #we process the whole row from the first cell (cell 0)
-      else
-        if not @teacher and @ignoreMissingTeacher
-          next
-        end
-        va = row[@aTet]
-        vb = row[@bTet]
-        vg = row[@grapta]
-        if ( !va and @checkA) || ( !vb and @checkB) || ( !vg and @checkC)
+          if @withDegree == 0
+            if not @missingTmimata[@tmima] 
+              @missingTmimata[@tmima] = []
+            end
+            @missingTmimata[@tmima] << @mathima unless @missingTmimata[@tmima].include? @mathima
 
-          @missingCounter += 1
-
-          missingStudent = {}
-          missingStudent[:am] = row[1]
-          missingStudent[:onoma] = row[2]
-          missingStudent[:epwnymo] = row[3]
-          missingStudent[:patrwnymo] = row[4]
-          missingStudent[:mitrwnymo] = row[5]
-          missingStudent[:tmima] = @tmima
-          missingStudent[:mathima] = @mathima
-          missingStudent[:tetramino] = @tetramino
-          missingStudent[:teacher] = @teacher
-          
-          fullname = "#{row[1]} #{row[2]} #{row[3]} #{row[4]} #{row[5]}"
-          if not missingStudents[fullname] 
-            missingStudents[fullname] = []
+            if not @missingLessons[@teacher] 
+              @missingLessons[@teacher] = []
+            end
+            @missingLessons[@teacher] << @tmima unless @missingLessons[@teacher].include? @tmima
           end
-          missingStudents[fullname] << @mathima unless missingStudents[fullname].include? @mathima
 
-          puts "Λείπει ο βαθμός(#{va} #{vb} #{vg}) του #{row[1]}, #{row[2]}, #{row[3]} του #{@tmima} στο #{@mathima}" if @debug
-
-          next if @missingCounter < 5
-
-          if not missingTmimata[@tmima] 
-            missingTmimata[@tmima] = []
-          end
-          missingTmimata[@tmima] << @mathima unless missingTmimata[@tmima].include? @mathima
-
-          if not missingLessons[@teacher] 
-            missingLessons[@teacher] = []
-          end
-          missingLessons[@teacher] << @tmima unless missingLessons[@teacher].include? @tmima
-
+          @tmima = @mathima = @teacher = nil
+        elsif i>0
+          next #we process the whole row from the first cell (cell 0)
         else
-          puts "ολα καλα" if @debug
+          if not @teacher and @ignoreMissingTeacher
+            next
+          end
+          va = row[@aTet]
+          vb = row[@bTet]
+          vg = row[@grapta]
+          if ( !va and @checkA) || ( !vb and @checkB) || ( !vg and @checkC)
+
+
+            missingStudent = {}
+            missingStudent[:am] = row[1]
+            missingStudent[:onoma] = row[2]
+            missingStudent[:epwnymo] = row[3]
+            missingStudent[:patrwnymo] = row[4]
+            missingStudent[:mitrwnymo] = row[5]
+            missingStudent[:tmima] = @tmima
+            missingStudent[:mathima] = @mathima
+            missingStudent[:tetramino] = @tetramino
+            missingStudent[:teacher] = @teacher
+            
+            fullname = "#{row[1]} #{row[2]} #{row[3]} #{row[4]} #{row[5]}"
+            if not @missingStudents[fullname] 
+              @missingStudents[fullname] = []
+            end
+            @missingStudents[fullname] << @mathima unless @missingStudents[fullname].include? @mathima
+
+            puts "Λείπει ο βαθμός(#{va} #{vb} #{vg}) του #{row[1]}, #{row[2]}, #{row[3]} του #{@tmima} στο #{@mathima}" if @debug
+
+          else
+            @withDegree += 1
+            puts "ολα καλα" if @debug
+          end
         end
       end
     end
   end
-end
-puts "Parsing finished"
-gets
+  puts "Parsing finished" if @debug
+  gets if @debug
 
-  missingStudents.each do |k, v|
+
+  @missingStudents.each do |k, v|
     if v.size>0
       print k, ":"
       puts v.size
@@ -135,10 +147,10 @@ gets
     end
   end
 
-  puts "hit enter"
-  gets
+  puts "hit enter" if @debug
+  gets if @debug
 
-  missingTmimata.each do |k, v|
+  @missingTmimata.each do |k, v|
     if v.size>0
       print k
       print " "
@@ -149,10 +161,10 @@ gets
     end
   end
 
-  puts "hit enter"
-  gets
+  puts "hit enter" if @debug
+  gets if @debug
 
-  missingLessons.each do |k,v|
+  @missingLessons.each do |k,v|
     if v.size>0
       print k
       print " " 
@@ -163,4 +175,7 @@ gets
     end
   end
 
+  haml :report
+
+end#post
 
