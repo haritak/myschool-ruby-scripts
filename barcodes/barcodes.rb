fd8edeef2f492b58135627adf2b0e8c4c54d401d
Charitakis Yannis <github@ych.gr>
2016-09-07 13:20:32 +0300

    Cleaned code

--- barcodes/barcodes.rb
+++ barcodes/barcodes.rb
@@ -1,21 +1,9 @@
-#require 'barby/barcode/code_39'
-#require 'barby/outputter/ascii_outputter'
-#require 'barby/outputter/png_outputter'
-#require 'barby/outputter/pdfwriter_outputter'
-#
-#barcode_data = "12345"
-#barcode39 = Barby::Code39.new(barcode_data) # Default value is false
-#barcode39_ext = Barby::Code39.new(barcode_data, true)
-#
-##puts barcode39_ext.to_ascii
-#
-## And if you want it PNG style
-#File.open('barcode39.png', 'w'){|f| f.write barcode39.to_png }
-#File.open('barcode39_ext.png', 'w'){|f| f.write barcode39_ext.to_png }
-
 require 'rqrcode'
 
-qrcode = RQRCode::QRCode.new("http://srv-1tee-moiron.ira.sch.gr/ktimatologio/1234567890")
+#https://github.com/whomwah/rqrcode
+
+qrcode = RQRCode::QRCode.new("http://srv-1tee-moiron.ira.sch.gr:45537/asset/1234567890")
+
 image = qrcode.as_png
 svg = qrcode.as_svg
 html = qrcode.as_html
@@ -26,4 +14,3 @@ IO.write("g2-qrcode.png", image.to_s)
 IO.write("g2-qrcode.svg", svg.to_s)
 IO.write("g2-qrcode.html", html.to_s)
 IO.write("g2-qrcode.txt", string)
-
