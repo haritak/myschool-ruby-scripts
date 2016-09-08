d67c11f3776cf03a930f006e23a39c732453b9de
Charitakis Yannis <github@ych.gr>
2016-09-08 13:09:05 +0300

    Found optimal size

---
+++ ettiketes/test.rb
@@ -0,0 +1,8 @@
+#https://github.com/madriska/prawn-labels
+require 'prawn/labels'
+
+names = ["Jordan", "Chris", "Jon", "Mike", "Kelly", "Bob", "Greg"]
+
+Prawn::Labels.generate("names.pdf", names, :type => "Avery5160") do |pdf, name|
+    pdf.text name
+end
