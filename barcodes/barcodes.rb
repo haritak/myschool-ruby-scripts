f0f0ff55c6977f10a49f43a760170d428df1ae50
Charitakis Yannis <github@ych.gr>
2016-09-12 11:28:39 +0300

    Previous tests

--- barcodes/barcodes.rb
+++ barcodes/barcodes.rb
@@ -4,13 +4,15 @@ require 'rqrcode'
 
 qrcode = RQRCode::QRCode.new("http://srv-1tee-moiron.ira.sch.gr:45537/asset/1234567890")
 
-image = qrcode.as_png
 svg = qrcode.as_svg
-html = qrcode.as_html
-string = qrcode.as_ansi
-string = qrcode.to_s
-
-IO.write("g2-qrcode.png", image.to_s)
 IO.write("g2-qrcode.svg", svg.to_s)
-IO.write("g2-qrcode.html", html.to_s)
-IO.write("g2-qrcode.txt", string)
+
+svg = qrcode.as_svg(module_size: 1)
+IO.write("g2-qrcode.5.svg", svg.to_s)
+
+svg = qrcode.as_svg(module_size: 0.1)
+IO.write("g2-qrcode.10.svg", svg.to_s)
+
+svg = qrcode.as_svg(module_size: 0.2)
+IO.write("g2-qrcode.15.svg", svg.to_s)
+
--- ettiketes/test.rb
+++ ettiketes/test.rb
@@ -3,6 +3,6 @@ require 'prawn/labels'
 
 names = ["Jordan", "Chris", "Jon", "Mike", "Kelly", "Bob", "Greg"]
 
-Prawn::Labels.generate("names.pdf", names, :type => "Avery5160") do |pdf, name|
+Prawn::Labels.generate("names.pdf", names, :type => "Avery18036") do |pdf, name|
     pdf.text name
 end
