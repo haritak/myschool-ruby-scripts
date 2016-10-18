7d28d99b1ef653e44ecb195335399ad7eb87ac1d
Charitakis Yannis <github@ych.gr>
2016-10-18 17:50:43 +0300

    Forgoten updates

--- barcodes/barcodes.rb
+++ barcodes/barcodes.rb
@@ -1,18 +1,31 @@
+#https://github.com/whomwah/rqrcode
 require 'rqrcode'
 
-#https://github.com/whomwah/rqrcode
+#https://github.com/toretore/barby
+require 'barby'
+require 'barby/barcode/code_128'
+require 'barby/outputter/png_outputter'
 
-qrcode = RQRCode::QRCode.new("http://srv-1tee-moiron.ira.sch.gr:45537/asset/1234567890")
 
-svg = qrcode.as_svg
-IO.write("g2-qrcode.svg", svg.to_s)
+encoded_string=[ "http://srv-1tee-moiron.ira.sch.gr:45537/asset/1234567890",
+"2016101825A3",
+"1210,1212",
+"YYYYMMDDCIDAMM01AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM02AMM35"]
 
-svg = qrcode.as_svg(module_size: 1)
-IO.write("g2-qrcode.5.svg", svg.to_s)
+encoded_string.each_with_index do |s,i|
+  qrcode = RQRCode::QRCode.new( s )
+  barcode = Barby::Code128B.new(s.encode("US-ASCII"))
+  File.open("barcode.#{i}.png", 'wb'){|f| f.write barcode.to_png(height:20)}
 
-svg = qrcode.as_svg(module_size: 0.1)
-IO.write("g2-qrcode.10.svg", svg.to_s)
+  svg = qrcode.as_svg
+  IO.write("qr.#{i}.svg", svg.to_s)
 
-svg = qrcode.as_svg(module_size: 0.2)
-IO.write("g2-qrcode.15.svg", svg.to_s)
+  svg = qrcode.as_svg(module_size: 1)
+  IO.write("qr.#{i}.5.svg", svg.to_s)
 
+  svg = qrcode.as_svg(module_size: 0.1)
+  IO.write("qr.#{i}.10.svg", svg.to_s)
+
+  svg = qrcode.as_svg(module_size: 0.2)
+  IO.write("qr.#{i}.15.svg", svg.to_s)
+end
--- ettiketes/generate.rb
+++ ettiketes/generate.rb
@@ -16,11 +16,11 @@ end
 Prawn::Labels.generate("names.pdf", names, :type => "Avery18036") do |pdf, name|
   qrcode = RQRCode::QRCode.new("http://srv-1tee-moiron.ira.sch.gr:45537/asset/#{name}")
   svg = qrcode.as_svg
-  IO.write("#{id}", svg.to_s)
+  IO.write("#{id}.svg", svg.to_s)
 
-  pdf.svg IO.read("#{name}"), at: [0,60], width: 50, height: 50
-  pdf.svg IO.read("#{name}"), at: [55,60], width: 50, height: 50
-  pdf.svg IO.read("#{name}"), at: [110,60], width: 50, height: 50
+  pdf.svg IO.read("#{name}.svg"), at: [0,60], width: 50, height: 50
+  pdf.svg IO.read("#{name}.svg"), at: [55,60], width: 50, height: 50
+  pdf.svg IO.read("#{name}.svg"), at: [110,60], width: 50, height: 50
   pdf.draw_text name, at: [1,1], size: 7
   pdf.draw_text name, at: [55,1], size: 7
   pdf.draw_text name, at: [110,1], size: 7
