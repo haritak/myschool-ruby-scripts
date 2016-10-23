a9e840bd932979f54ecf026f887d2e5035b3a5c8
administrator@srv-1tee-moiron <administrator@srv-1tee-moiron>
2016-10-23 08:50:53 +0300

    fixed script to start one shell with two windows

--- list-monitoring/startListMonitoring.sh
+++ list-monitoring/startListMonitoring.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-tmux new -s artemis -d
+tmux new-session -s artemis -d
 tmux send-keys -t artemis 'cd /home/administrator/myschool-ruby-scripts/list-monitoring' C-m
 tmux send-keys -t artemis 'ruby processGroupMembers.rb' C-m
-tmux send-keys -t artemis C-c 'cd /home/administrator/myschool-ruby-scripts/list-monitoring' C-m
+tmux new-window -t artemis
+tmux send-keys -t artemis 'cd /home/administrator/myschool-ruby-scripts/list-monitoring' C-m
 tmux send-keys -t artemis 'while true; do ruby prepareGroups.rb ; echo "waiting";sleep 120; done' C-m
