dc3e50a9240a27f9a3c35396d0ca87252c9a8003
Charitakis Yannis <github@ych.gr>
2016-10-26 16:49:31 +0300

    Adding support to publish the program

--- list-monitoring/prepareGroups.rb
+++ list-monitoring/prepareGroups.rb
@@ -1,10 +1,14 @@
 #
 #https://github.com/mikel/mail
 #
+require 'pry'
 require 'roo-xls'
 
 require 'mail'
 load "forbiden"
+
+ME = "artemis1epalmoiron@gmail.com"
+TESTING = true #will send emails to me 
 
 programmers = ["charitakis.ioannis@gmail.com",
                "tkodellas@gmail.com",
@@ -29,10 +33,85 @@ Mail.defaults do
                   enable_starttls_auto: true)
 end
 
+def informSchedulersOfMissing(email, missingFiles)
+  missing_files = missingFiles.join(",")
+  Mail.deliver do
+    charset = "UTF-8"
+    content_transfer_encoding="8bit"
+    from 'Αρτέμης Μάτσας <artemis1epalmoiron@gmail.com>'
+    if not TESTING
+      to 'tkodellas@gmail.com'
+      cc 'charitakis.ioannis@gmail.com'
+    else
+      to 'charitakis.ioannis@gmail.com'
+      cc 'bp10.charitakis@gmail.com'
+      cc 'tmp123@ych.gr'
+    end
+    subject 'Λείπουν αρχεία!'
+    text_part do
+      content_type "text/plain; charset=utf-8"
+      body <<-EOF
+Γειά σας,
+
+Πολύ πρόσφατα πήρα απο εσάς ένα email με τίτλο:
+'#{email.subject}'
+το οποίο για την ακρίβεια έχει σταλεί από :
+'#{email.from}'
+και από τα παραπάνω υπέθεσα ότι περιέχει το Ωρολόγιο Πρόγραμμα.
+
+Λυπάμαι αλλά πρέπει να σας ενημερώσω ότι απο τα αρχεία για το ωρολόγιο
+πρόγραμμα που στέιλατε λείπουν τα παρακάτω:
+'#{missing_files}'
+
+Παρακαλώ επαναλάβατε την αποστολή του ωρολόγιου,
+συμπεριλαμβάνοντας ότι λείπει για να μπορώ να δουλέψω και εγώ.
+
+Με τιμή,
+Αρτέμης Μάτσας
+      EOF
+    end
+  end
+end
+
+def emailFile(filename)
+  Mail.deliver do
+    charset = "UTF-8"
+    content_transfer_encoding="8bit"
+    from 'Αρτέμης Μάτσας <artemis1epalmoiron@gmail.com>'
+    if TESTING
+      to 'charitakis.ioannis@gmail.com'
+    else 
+      to '1epal-moiron-dev@googlegroups.com'
+    end
+    time = Time.new
+    subject "Ετοιμο αρχείο READY.xls για αυτά που πήρα στις #{time.day}-#{time.month}"
+    add_file filename
+    text_part do
+      content_type "text/plain; charset=utf-8"
+      body <<-EOF
+Γειά σας,
+
+Είμαι στην ευχάριστη θέσης να σας στείλω τα groupakia.
+Χρειάζονται λίγη δουλίτσα ακόμα.
+
+Μπορείτε να ρυθμίσετε ποιοι θα είναι στα group εδώ:
+http://srv-1tee-moiron.ira.sch.gr:4567/ αλλά
+μετά θα πρέπει να μου στείλετε (σε εμένα, όχι σε λίστα) άλλο ένα email
+με συννημμένο το αρχείο EXCEL.xls 
+έτσι ώστε να ξαναπιάσω δουλειά.
+
+
+Με τιμή,
+Αρτέμης Μάτσας
+      EOF
+    end
+  end
+end
+
 Mail.all.each do |m|
 
   sender = m.from
-  recipients = m.to + m.cc + m.bcc
+  recipients = "#{m.to} #{m.cc} #{m.bcc}"
   puts
   puts "-----"
   puts sender
@@ -41,15 +120,33 @@ Mail.all.each do |m|
 
 
   if programmers.map{|p| sender.include? p}.include?(true)
-    puts "sent from programmers"
+    puts "This email was sent from one of the persons that works on the schedule"
 
-    titleHintsSchedule = (m.subject =~ /^\[Λίστα ΕΠΑΛ\].*ρολ.γιο.*/)
+    personal = recipients.include?(ME)
+    puts "This email was specifically sent to me" if personal
+
+    titleHintsSchedule = (m.subject =~ /.*ρολ.γιο.*ρ.γραμ.*/)
     puts "Title hints this is the schedule" if titleHintsSchedule
 
     xlsFound = false
+    foundFiles = []
+    notFoundFiles = [
+      "EXCEL.xls",
+      "TEACHERS.pdf",
+      "TEACHERS_DETAILED.pdf",
+      "STUDENTS.pdf",
+      "STUDENTS_DETAILED.pdf",
+      "ROOMS.pdf",
+      "ROOMS_DETAILED.pdf",
+      "KATANOMI.xls",
+      "roz file (AscTimetables file)"]
+
 
     m.attachments.each do |a|
       filename = a.filename
+      puts "New attachment #{filename}"
+      foundFiles << notFoundFiles.select { |f| filename =~ /#{Regexp.escape(f)}/ } 
+      foundFiles << "roz file (AscTimetables file)" if filename =~ /.*roz/
       if filename =~ /EXCEL\.xls/
         xlsFound = true
         puts "Schedule EXCEL.xls found. Saving"
@@ -66,76 +163,31 @@ Mail.all.each do |m|
 
         %x{ ruby filterTeachers.rb }
         %x{ cd GroupFixer && php groupfixer.php > READY.xls }
-	sleep 15
+        puts "Waiting 5 seconds for previous operation to finish"
+        sleep 5
         if File.exist?('GroupFixer/READY.xls')
-          Mail.deliver do
-            charset = "UTF-8"
-            content_transfer_encoding="8bit"
-            from 'Αρτέμης Μάτσας <artemis1epalmoiron@gmail.com>'
-            to '1epal-moiron-dev@googlegroups.com'
-            time = Time.new
-            subject "Ετοιμο αρχείο READY.xls για αυτά που πήρα στις #{time.day}-#{time.month}"
-            add_file 'GroupFixer/READY.xls'
-            text_part do
-              content_type "text/plain; charset=utf-8"
-              body <<-EOF
-Γειά σας,
-
-Είμαι στην ευχάριστη θέσης να σας στείλω τα groupakia.
-Χρειάζονται λίγη δουλίτσα ακόμα.
-
-Μπορείτε να ρυθμίσετε ποιοι θα είναι στα group εδώ:
-http://srv-1tee-moiron.ira.sch.gr:4567/ αλλά
-μετά θα πρέπει να μου στείλετε (σε εμένα, όχι σε λίστα) άλλο ένα email
-με συννημμένο το αρχείο EXCEL.xls 
-έτσι ώστε να ξαναπιάσω δουλειά.
-
-
-Με τιμή,
-Αρτέμης Μάτσας
-              EOF
-            end
-          end
+          emailFile('GroupFixer/READY.xls')
+        else
+          puts "TODO - κάτι στράβωσε στην δημιουργία τους READY.xls"
         end
-      else
-        puts "ignoring attachment"
-      end
-    end
-    if titleHintsSchedule and !xlsFound
-      puts "Missing EXCEL.xls from schedule!"
-      next
-      Mail.deliver do
-        charset = "UTF-8"
-        content_transfer_encoding="8bit"
-        from 'Αρτέμης Μάτσας <artemis1epalmoiron@gmail.com>'
-        to 'tkodellas@gmail.com'
-        cc 'charitakis.ioannis@gmail.com'
-        subject 'Λείπει το αρχείο EXCEL.xls'
-        text_part do
-          content_type "text/plain; charset=utf-8"
-          body <<-EOF
-Γειά σας,
+      end #matches xls
+    end # each attachment
 
-Πολύ πρόσφατα πήρα απο εσάς ένα email με τίτλο:
-'#{m.subject}'
-το οποίο για την ακρίβεια έχει σταλεί από :
-'#{m.from}
-και από τα παραπάνω υπέθεσα ότι περιέχει το Ωρολόγιο Πρόγραμμα.
+    foundFiles.flatten!
 
-Λυπάμαι αλλά πρέπει να σας ενημερώσω ότι απο τα αρχεία για το ωρολόγιο
-πρόγραμμα που στέιλατε λείπει το EXCEL.xls και υπάρχει πρόβλημα.
+    notFoundFiles = notFoundFiles - foundFiles
 
-Παρακαλώ επαναλάβατε την αποστολή του ωρολόγιου,
-συμπεριλαμβάνοντας και το αντίστοιχο EXCEL.xls για να μπορώ να 
-δουλέψω και εγώ.
-
-Με τιμή,
-Αρτέμης Μάτσας
-          EOF
-        end
-      end
+    if titleHintsSchedule and notFoundFiles.size!=0
+      puts "Missing files"
+      puts notFoundFiles
+      informSchedulersOfMissing(m, notFoundFiles)
+    else
+      puts "ALL FILES IN PLACE!"
+      puts "PUBLISH THE PROGRAM AND INFORM THE SCHEDULERS"
     end
   end
 end
 
-#Mail.find_and_delete
+if not TESTING
+  Mail.find_and_delete
+end
