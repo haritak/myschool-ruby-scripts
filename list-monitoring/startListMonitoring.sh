ad769fd3c36335a32b30a62974472a4adf0e4b0f
administrator@srv-1tee-moiron <administrator@srv-1tee-moiron>
2016-10-26 14:42:12 +0300

    Raized the waiting time from 120seconds to 300seconds

--- list-monitoring/startListMonitoring.sh
+++ list-monitoring/startListMonitoring.sh
@@ -4,4 +4,4 @@ tmux send-keys -t artemis 'cd /home/administrator/myschool-ruby-scripts/list-mon
 tmux send-keys -t artemis 'ruby processGroupMembers.rb' C-m
 tmux new-window -t artemis
 tmux send-keys -t artemis 'cd /home/administrator/myschool-ruby-scripts/list-monitoring' C-m
-tmux send-keys -t artemis 'while true; do ruby prepareGroups.rb ; echo "waiting";sleep 120; done' C-m
+tmux send-keys -t artemis 'while true; do ruby prepareGroups.rb ; echo "waiting";sleep 300; done' C-m
